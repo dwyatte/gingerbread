325d1606e2859693ff99bf665ee7b4378bf18272
dean.wyatte <dean.wyatte@logrhythm.com>
2017-02-05 18:03:21 -0700

    use X11 acl instead

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# start the socket, trap pid for exit
-socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-pid=$!
-trap 'kill $pid' EXIT
-
-# get local ip and forward to container
+# add ip to XQuartz acl
 ip=$(python -c 'import socket; print(socket.gethostbyname(socket.gethostname()))')
+xhost + $ip
+trap 'xhost - $ip' EXIT
+
+# forward to container
 docker run -it --rm -p 8888:8888 -p 6006:6006 -v=$PWD:$PWD -w=$PWD -e DISPLAY=$ip:0 --name=tensorflow-cpu tensorflow-cpu /bin/bash
